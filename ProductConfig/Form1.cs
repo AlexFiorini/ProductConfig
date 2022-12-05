6c28875eee07174caf3bed56083cb0066b626f8e
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2022-12-05 15:48:28 +0100

    Test

--- ProductConfig/Form1.cs
+++ ProductConfig/Form1.cs
@@ -1,10 +1,11 @@
 using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ProductConfig
 {
     public partial class Form1 : Form
     {
-        String savePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         public Form1()
         {
@@ -13,12 +14,8 @@ namespace ProductConfig
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = savePath;
-            if(saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                richTextBox1.SaveFile(saveFileDialog.FileName);
-            }
+
+
         }
     }
 }
